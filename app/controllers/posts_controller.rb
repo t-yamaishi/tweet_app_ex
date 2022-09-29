4d2f5667db4ed04dddbd8a998f720e55f40cdd3e
yamaishi <yamakios8@icloud.com>
2022-09-29 18:12:03 +0900

    バリテーション作成、エラーメッセージが表示される

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,8 +13,12 @@ class PostsController < ApplicationController
   end
 
   def create
-    Post.create(post_params)
-    redirect_to new_post_path
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to new_post_path
+    else
+      render :new
+    end
   end
 
   private
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,3 @@
 class Post < ApplicationRecord
+  validates :content, presence: true,length: { minimum: 1 }, length: { maximum: 140 }
 end
