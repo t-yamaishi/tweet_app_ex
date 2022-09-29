354c1b3fda13c02d50bfcf7434fc9ddfa58deccb
yamaishi <yamakios8@icloud.com>
2022-09-29 18:01:50 +0900

    showアクション作成、詳細画面が表示される

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,9 +3,15 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def show
+   @post = Post.find(params[:id])
+  end
+
   def new
     @post = Post.new
   end
+
   def create
     Post.create(post_params)
     redirect_to new_post_path
