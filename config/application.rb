34530770c59c170ac06083858d6b68c4d1ab24bf
yamaishi <yamakios8@icloud.com>
2022-10-12 23:19:42 +0900

    helperとassetsファイルを生成されないようにする

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,10 @@ module TweetAppEx
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
 
+    config.generators do |g|
+      g.assets     false
+      g.helper     false
+    end
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
